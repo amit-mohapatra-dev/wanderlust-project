7c175ccd396c3780bbf3db641fad9dcfbe273c52
Amit Mohapatra <amitmohapatra.dev@gmail.com>
2026-04-17 21:31:29 +0530

    Initial commit

---
+++ init/index.js
@@ -0,0 +1,36 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
+
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const dbUrl = process.env.ATLASDB_URL;
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(dbUrl);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "65f498de938bbbafdcf350b8",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
